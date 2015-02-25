24af32a154ce9cf18fa567e9646fc5dc0e8b4321
Student <student@swope252-01.wcupa.net>
2015-02-25 14:25:44 -0500

    Initial Commit

---
+++ MIDI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MIDI
+//
+//  Created by Student on 2/25/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
